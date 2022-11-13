81f56bc52cd6decd0a9e3c500942df1e30a69ad1
Renij Shrestha <renijshrestha8@gmail.com>
2022-11-13 17:47:36 +1100

    functions

--- src/components/cvFrom/Education.js
+++ src/components/cvFrom/Education.js
@@ -4,7 +4,15 @@ export default class education extends Component {
   constructor() {
     super();
 
-    this.state = { education: [], edit: false };
+    this.state = {
+      school: "",
+      degree: "",
+      location: "",
+      endDate: "",
+      desc: "",
+      education: [],
+      edit: false,
+    };
   }
 
   handleEduEdit = () => {
@@ -13,38 +21,129 @@ export default class education extends Component {
     }));
   };
 
+  handleSchoolChange = (e) => {
+    this.setState({
+      school: e.target.value,
+    });
+  };
+
+  handleDegreeChange = (e) => {
+    this.setState({
+      degree: e.target.value,
+    });
+  };
+
+  handleLocationChange = (e) => {
+    this.setState({
+      location: e.target.value,
+    });
+  };
+
+  handleEndDateChange = (e) => {
+    this.setState({
+      endDate: e.target.value,
+    });
+  };
+
+  handleDescChange = (e) => {
+    this.setState({
+      desc: e.target.value,
+    });
+  };
+
+  handleEduSubmit = (e) => {
+    this.setState({
+      education: this.state.education.push({
+        school: this.state.school,
+        degree: this.state.degree,
+        location: this.state.location,
+        endDate: this.state.endDate,
+        desc: this.state.desc,
+      }),
+      school: "",
+      degree: "",
+      location: "",
+      endDate: "",
+      desc: "",
+    });
+  };
+
   render() {
-    const { education, edit } = this.state;
+    const { school, degree, location, endDate, desc, education, edit } =
+      this.state;
     return (
       <div>
         <h2>education</h2>
         <hr />
         <button onClick={this.handleEduEdit}>+ education</button>
+        <section>
+          <div className="first row">
+            <span className="leftSide">University</span>
+            <span className="rightSide">Location</span>
+          </div>
+          <div className="second row">
+            <span className="leftSide">Degree</span>
+            <span className="rightSide">End Date</span>
+          </div>
+          <div className="third row">
+            <span>desc</span>
+          </div>
+        </section>
         {edit && (
           <form id="edu-edit" className="edit-form">
             <div>
               <label htmlFor="school">School:</label>
-              <input type="text" id="school" required />
+              <input
+                type="text"
+                id="school"
+                onChange={this.handleSchoolChange}
+                value={school}
+                required
+              />
             </div>
             <div>
               <label htmlFor="degree">Degree:</label>
-              <input type="text" id="degree" />
+              <input
+                type="text"
+                id="degree"
+                value={degree}
+                onChange={this.handleDegreeChange}
+              />
             </div>
             <div>
               <label htmlFor="location">Location:</label>
-              <input type="text" id="location" />
+              <input
+                type="text"
+                id="location"
+                value={location}
+                onChange={this.handleLocationChange}
+              />
             </div>
             <div>
               <label htmlFor="end">End Date:</label>
-              <input type="Date" id="end" />
+              <input
+                type="Date"
+                id="end"
+                onChange={this.handleEndDateChange}
+                value={endDate}
+              />
             </div>
             <div>
               <label htmlFor="desc">Description</label>
               <br />
-              <textarea name="desc" id="desc" cols="50" rows="5"></textarea>
+              <textarea
+                name="desc"
+                id="desc"
+                cols="50"
+                rows="5"
+                onChange={this.handleDescChange}
+                value={desc}
+              ></textarea>
             </div>
             <div>
-              <button type="submit">Add</button>
+              <button type="button" onClick={this.handleEduSubmit}>
+                Add
+              </button>
               <button type="button" onClick={this.handleEduEdit}>
                 Cancel
               </button>
---
+++ src/components/cvFrom/Experience.js
@@ -0,0 +1,174 @@
+import React, { Component } from "react";
+
+export default class experience extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      company: "",
+      location: "",
+      position: "",
+      start: "",
+      end: "",
+      desc: "",
+      experiences: [],
+      edit: false,
+    };
+  }
+
+  handleExpEdit = () => {
+    this.setState((prevState) => ({
+      edit: !prevState.edit,
+    }));
+  };
+
+  handleCompanyChange = (e) => {
+    this.setState(() => ({
+      company: e.target.value,
+    }));
+  };
+
+  handleLocationChange = (e) => {
+    this.setState(() => ({
+      location: e.target.value,
+    }));
+  };
+
+  handlePositionChange = (e) => {
+    this.setState(() => ({
+      position: e.target.value,
+    }));
+  };
+
+  handleStartChange = (e) => {
+    this.setState(() => ({
+      start: e.target.value,
+    }));
+  };
+
+  handleEndChange = (e) => {
+    this.setState(() => ({
+      end: e.target.value,
+    }));
+  };
+
+  handleDescChange = (e) => {
+    this.setState(() => ({
+      desc: e.target.value,
+    }));
+  };
+
+  handleExpSubmit = () => {
+    this.setState((prevState) => ({
+      expriences: prevState.experiences.push({
+        company: this.state.company,
+        location: this.state.location,
+        position: this.state.position,
+        start: this.state.start,
+        end: this.state.end,
+        desc: this.state.desc,
+      }),
+      company: "",
+      location: "",
+      position: "",
+      start: "",
+      end: "",
+      desc: "",
+    }));
+  };
+
+  render() {
+    const { company, location, position, start, end, desc, experiences, edit } =
+      this.state;
+    return (
+      <div>
+        <h2>exprience</h2>
+        <hr />
+        <section>
+          <div className="first row">
+            <span className="leftSide">Company</span>
+            <span className="rightSide">Location</span>
+          </div>
+          <div className="second row">
+            <span className="leftSide">Position</span>
+            <span className="rightSide">Start - End</span>
+          </div>
+          <div className="third row">
+            <span>desc</span>
+          </div>
+        </section>
+        <button onClick={this.handleExpEdit}>+ experience</button>
+        {edit && (
+          <form id="exp-edit" className="edit-form">
+            <div>
+              <label htmlFor="company">Company:</label>
+              <input
+                type="text"
+                id="company"
+                onChange={this.handleCompanyChange}
+                value={company}
+                required
+              />
+            </div>
+            <div>
+              <label htmlFor="position">Position:</label>
+              <input
+                type="text"
+                id="position"
+                onChange={this.handlePositionChange}
+                value={position}
+              />
+            </div>
+            <div>
+              <label htmlFor="location">Location:</label>
+              <input
+                type="text"
+                id="location"
+                onChange={this.handleLocationChange}
+                value={location}
+              />
+            </div>
+            <div>
+              <label htmlFor="start">Start Date:</label>
+              <input
+                type="Date"
+                id="start"
+                onChange={this.handleStartChange}
+                value={start}
+              />
+            </div>
+            <div>
+              <label htmlFor="end">End Date:</label>
+              <input
+                type="Date"
+                id="end"
+                onChange={this.handleEndChange}
+                value={end}
+              />
+            </div>
+            <div>
+              <label htmlFor="desc">Description</label>
+              <br />
+              <textarea
+                name="desc"
+                id="desc"
+                cols="50"
+                rows="5"
+                onChange={this.handleDescChange}
+                value={desc}
+              ></textarea>
+            </div>
+            <div>
+              <button type="button" onClick={this.handleExpSubmit}>
+                Add
+              </button>
+              <button type="button" onClick={this.handleExpEdit}>
+                Cancel
+              </button>
+            </div>
+          </form>
+        )}
+      </div>
+    );
+  }
+}
