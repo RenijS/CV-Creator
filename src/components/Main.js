c6fb1dc808b9c9d14e91b41a19cee8bacb20f550
Renij Shrestha <renijshrestha8@gmail.com>
2022-11-21 08:39:33 +1100

    modes

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,7 +2,42 @@ import React, { Component } from "react";
 import CvForm from "./cvFrom/CvForm";
 
 export default class main extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      previewMode: false,
+    };
+
+    this.workingMode = this.workingMode.bind(this);
+    this.previewMode = this.previewMode.bind(this);
+  }
+
+  workingMode = () => {
+    this.setState({
+      previewMode: false,
+    });
+    for (let plusBtn of document.querySelectorAll(".plusBtn")) {
+      plusBtn.classList.remove("displayGone");
+    }
+    document.querySelector(".previewBtn").classList.remove("active");
+    document.querySelector(".workingBtn").classList.add("active");
+  };
+
+  previewMode = () => {
+    this.setState({
+      previewMode: true,
+    });
+    for (let plusBtn of document.querySelectorAll(".plusBtn")) {
+      plusBtn.classList.add("displayGone");
+    }
+    document.querySelector(".previewBtn").classList.add("active");
+    document.querySelector(".workingBtn").classList.remove("active");
+  };
+
   render() {
-    return <CvForm />;
+    return (
+      <CvForm workingMode={this.workingMode} previewMode={this.previewMode} />
+    );
   }
 }
